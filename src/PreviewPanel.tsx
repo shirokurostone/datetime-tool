b3d47e9a0649dab7e2603909c72b3a6c59f7bf11
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2021-10-25 00:08:37 +0900

    !==に修正

--- src/PreviewPanel.tsx
+++ src/PreviewPanel.tsx
@@ -42,7 +42,7 @@ export function parse(inputs: NodeInfo[], regex: RegExp|string) : NodeInfo[]{
     let lastIndex = 0;
     let match;
     while ((match = re.exec(input.text)) !== null){
-      if (match.index !== 0 && lastIndex != match.index){
+      if (match.index !== 0 && lastIndex !== match.index){
         result.push({
           text: input.text.slice(lastIndex, match.index),
           type: 'text',
