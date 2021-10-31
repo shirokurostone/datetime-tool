a32f7fb87ef5c9eb84ad752e756f46a21dc799a5
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2021-10-31 13:38:11 +0900

    TimestampをPreviewPanelからAppに渡すように修正

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ type AppState = {
 }
 
 class App extends React.Component<AppProps, AppState>{
-  constructor(props: AppProps){
+  constructor(props: AppProps) {
     super(props);
     this.state = {
       timestamps: [],
@@ -30,11 +30,11 @@ class App extends React.Component<AppProps, AppState>{
     this.handleAddTimestamp = this.handleAddTimestamp.bind(this);
   }
 
-  handleChangeTimestamp(id: number, value: Timestamp){
+  handleChangeTimestamp(id: number, value: Timestamp) {
     this.setState({
-      timestamps: this.state.timestamps.map((timestamp,i)=>{
-        if (id === timestamp.id){
-          const t = {...timestamp};
+      timestamps: this.state.timestamps.map((timestamp, i) => {
+        if (id === timestamp.id) {
+          const t = { ...timestamp };
           t.time = value;
           return t;
         }
@@ -43,60 +43,60 @@ class App extends React.Component<AppProps, AppState>{
     });
   }
 
-  handleRemoveTimestamp(id: number){
+  handleRemoveTimestamp(id: number) {
     this.setState({
-      timestamps: this.state.timestamps.filter(v=>v.id !== id),
+      timestamps: this.state.timestamps.filter(v => v.id !== id),
     });
   }
 
-  handleClick(event: React.MouseEvent<HTMLButtonElement>){
-    const id = this.state.maxId+1;
+  handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    const id = this.state.maxId + 1;
     const tm = Timestamp.now();
-    this.state.timestamps.push({id:id, time: tm, defaultText: tm.format('default')});
+    this.state.timestamps.push({ id: id, time: tm, defaultText: tm.format('default') });
     this.setState({
       timestamps: this.state.timestamps,
       maxId: id,
     });
   }
 
-  handleAddTimestamp(text:string){
-    const id = this.state.maxId+1;
-    this.state.timestamps.push({id:id, time: Timestamp.parse(text, false), defaultText: text});
+  handleAddTimestamp(text: string, timestamp: Timestamp) {
+    const id = this.state.maxId + 1;
+    this.state.timestamps.push({ id: id, time: timestamp, defaultText: text });
     this.setState({
       timestamps: this.state.timestamps,
       maxId: id,
     });
   }
 
-  render(){
+  render() {
     let elements: JSX.Element[] = [];
-    for (let i=0; i<this.state.timestamps.length; i++){
-      elements.push( (
+    for (let i = 0; i < this.state.timestamps.length; i++) {
+      elements.push((
         <TimestampPanel
           key={this.state.timestamps[i].id}
           id={this.state.timestamps[i].id}
-          onChange={(id, value)=>this.handleChangeTimestamp(id, value)}
-          onRemove={(id)=>this.handleRemoveTimestamp(id)}
+          onChange={(id, value) => this.handleChangeTimestamp(id, value)}
+          onRemove={(id) => this.handleRemoveTimestamp(id)}
           time={this.state.timestamps[i].time}
-          defaultTimestampText={this.state.timestamps[i].defaultText}/>
-      ) );
-      if (i !== this.state.timestamps.length-1 ){
-        elements.push( (
+          defaultTimestampText={this.state.timestamps[i].defaultText} />
+      ));
+      if (i !== this.state.timestamps.length - 1) {
+        elements.push((
           <DurationPanel
-            key={this.state.timestamps[i].id+"_duration"}
+            key={this.state.timestamps[i].id + "_duration"}
             from={this.state.timestamps[i].time}
-            to={this.state.timestamps[i+1].time}/>
-        ) );
+            to={this.state.timestamps[i + 1].time} />
+        ));
       }
     }
 
     return (
       <div className="App container-xxl">
-        <PreviewPanel onAddTimestamp={this.handleAddTimestamp}/>
+        <PreviewPanel onAddTimestamp={this.handleAddTimestamp} />
         <div className="">
-          { elements }
+          {elements}
           <div className="row plusbutton-card">
-            <button className="btn btn-outline-secondary col-12" onClick={this.handleClick}><PlusLgIcon/></button>
+            <button className="btn btn-outline-secondary col-12" onClick={this.handleClick}><PlusLgIcon /></button>
           </div>
         </div>
       </div>
--- src/PreviewPanel.tsx
+++ src/PreviewPanel.tsx
@@ -1,48 +1,50 @@
 import React, { useState } from 'react';
-import {Parser} from './Parser';
+import { Parser } from './Parser';
 import './PreviewPanel.css';
+import { Timestamp } from './Timestamp';
 
 type TimestampNodeProps = {
-　text: string,
-  onClick: (text:string)=>void,
+  text: string,
+  timestamp: Timestamp,
+  onClick: (text: string, timestamp: Timestamp) => void,
 }
-    
-function TimestampNode(props: TimestampNodeProps){
+
+function TimestampNode(props: TimestampNodeProps) {
   return (
-    <a href="#" className="preview-timestamp-node link-primary" onClick={()=>props.onClick(props.text)}>{props.text}</a>
+    <a href="#" className="preview-timestamp-node link-primary" onClick={() => props.onClick(props.text, props.timestamp)}>{props.text}</a>
   );
 }
 
 type TextNodeProps = {
   text: string,
 }
-  
-function TextNode(props: TextNodeProps){
+
+function TextNode(props: TextNodeProps) {
   return (
     <span className="preview-text-node">{props.text}</span>
   );
 }
 
 type PreviewPanelProps = {
-  onAddTimestamp: (text:string)=>void,
+  onAddTimestamp: (text: string, timestamp: Timestamp) => void,
 }
 
-export function PreviewPanel(props: PreviewPanelProps){
+export function PreviewPanel(props: PreviewPanelProps) {
   const [inputText, setInputText] = useState("");
 
   let id = 0;
-  const parser = new Parser(()=>new Date());
-  const result:JSX.Element[] =parser.parse(inputText).map(
-    token=>{
-        switch (token.type){
-          case 'text':
-            return (<TextNode key={id++} text={token.text}/>);
-          case 'timestamp':
-            return (<TimestampNode key={id++} text={token.text} onClick={props.onAddTimestamp}/>);
-          case 'lf':
-            return (<br key={id++}/>);
-        }
+  const parser = new Parser(() => new Date());
+  const result: JSX.Element[] = parser.parse(inputText).map(
+    token => {
+      switch (token.type) {
+        case 'text':
+          return (<TextNode key={id++} text={token.text} />);
+        case 'timestamp':
+          return (<TimestampNode key={id++} text={token.text} timestamp={token.timestamp} onClick={props.onAddTimestamp} />);
+        case 'lf':
+          return (<br key={id++} />);
       }
+    }
   );
 
   return (
@@ -50,16 +52,16 @@ export function PreviewPanel(props: PreviewPanelProps){
       <div className="card-body">
         <div className="row">
           <div className="col-6">
-          <textarea
-            className="form-control preview-textarea"
-            rows={10}
-            onChange={(e)=>{setInputText(e.target.value)}}
-            value={inputText}
+            <textarea
+              className="form-control preview-textarea"
+              rows={10}
+              onChange={(e) => { setInputText(e.target.value) }}
+              value={inputText}
             />
           </div>
           <div className="col-6">
             <div className="preview-column">
-              { result }
+              {result}
             </div>
           </div>
         </div>
