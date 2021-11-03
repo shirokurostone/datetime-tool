f5012c6c4049c423aa0d70f99e6de2c48abfedc9
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2021-11-03 12:33:12 +0900

    HTML, css修正

--- src/DurationPanel.tsx
+++ src/DurationPanel.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Timestamp, Duration } from './Timestamp'
+import { ReactComponent as StopwatchIcon } from 'bootstrap-icons/icons/stopwatch.svg';
 
 type DurationPanelProps = {
   from: Timestamp,
@@ -10,8 +11,11 @@ function DurationPanel(props: DurationPanelProps) {
   const duration = new Duration(props.from, props.to);
   return (
     <div className="shadow-sm rounded bg-body border my-3">
-      <div className="card-body">
-        {duration.toDurationString()}
+      <div className="row p-3 d-flex flex-row align-items-center">
+        <div className="col-1"><StopwatchIcon /></div>
+        <div className="col-2 d-flex flex-row">
+          <input className="form-control-plaintext" type="text" value={duration.toDurationString()} readOnly />
+        </div>
       </div>
     </div>
   );
